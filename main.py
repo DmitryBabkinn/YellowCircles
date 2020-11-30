f64dc9c31f3f6449352e1c36ce1285a0076a78e6
DmitryBabkinn <dmitrybab80@mail.ru>
2020-11-30 20:51:06 +0300

    --Form --GUI

---
+++ main.py
@@ -0,0 +1,19 @@
+import csv
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
